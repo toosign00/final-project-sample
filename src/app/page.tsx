bd9b26da57ab180bb8831795d009e4e1cbe68446
toosign00 <kevinsoo1014@gmail.com>
2025-07-07 01:23:29 +0000

    폴더명 변경(오탈자 수정)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 export default function Home() {
   return (
-    <h1>Final-Project-Sample</h1>
+    <h1>Final-Project-Sample, build and deploy test!!<br/>
+    {new Date().toLocaleDateString()}</h1>
   );
 }
